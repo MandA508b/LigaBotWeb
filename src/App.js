0c844b9a2b76b499b71e1496cde064db8cd41ff4
m4xx1k <maxikfabin@gmail.com>
2023-01-28 03:42:47 +0200

    close button, info 'bout user

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
 
 function App() {
+  const {tg,user,onClose} = useTelegram()
+  useEffect(()=>{
+      tg.ready()
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={onClose}>close</button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            {JSON.stringify(user,null, 2)}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }

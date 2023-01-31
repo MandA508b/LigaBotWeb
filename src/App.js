18694f0fe1daa829ee6ef4f513b650dfd24c80ef
m4xx1k <maxikfabin@gmail.com>
2023-01-31 05:24:02 +0200

    uncommit test form14

--- src/App.js
+++ src/App.js
@@ -252,7 +252,7 @@ function App() {
             <button className={'close_btn'} onClick={()=>setShowRes(prev=>!prev)}>Показати/Скрити Результат</button>
             <p>
                 {showRes ? JSON.stringify({
-                    type,city,amount,isPartly,percent,deadline,additionalInfo, user:user.username
+                    type,city,amount,isPartly,percent,deadline,additionalInfo, ...user
                 }, null,2) : '-'}
             </p>
             <button className={'close_btn'} onClick={onClose}>Закрити</button>

71281f8183f33d6ef9f32e664a9191eaa63dba5b
m4xx1k <maxikfabin@gmail.com>
2023-01-31 14:38:06 +0200

    final test26

--- src/pages/Redact.jsx
+++ src/pages/Redact.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {useLocation} from "react-router-dom";
 
 const Redact = () => {
+    const location = useLocation()
     return (
         <div>
-            
+            {location.pathname}
         </div>
     );
 };

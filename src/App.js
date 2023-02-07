d045864a085d6f5302d90764d822d9542e21a585
MandA508b <123251306+MandA508b@users.noreply.github.com>
2023-02-07 17:29:38 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Route, Routes} from "react-router-dom";
 import Create from "./pages/Create";
 import Redact from "./pages/Redact";
-const url = 'https://ligabotv2.onrender.com'
+const url = 'http://185.25.118.136:5000'
 const App = () => {
     return (
         <Routes>

d2d6f49b2fdc859f492821cab1651ea18299ef96
MandA508b <123251306+MandA508b@users.noreply.github.com>
2023-02-20 02:40:20 +0200

    fixed link to rate_request

--- src/App.js
+++ src/App.js
@@ -9,12 +9,13 @@ import Redact from "./pages/Redact";
 import YourPrice from "./pages/YourPrice/YourPrice";
 
 const url = 'https://ligabotv2.onrender.com'
+const chat_url = 'https://chat-server-zsxa.onrender.com'
 const App = () => {
     return (
         <Routes>
             <Route path={'/'} element={<Create url={url}/>}/>
             <Route path={'/redact/:id'} element={<Redact url={url}/>}/>
-            <Route path={'/rate'} element={<YourPrice url={url}/>}/>
+            <Route path={'/rate'} element={<YourPrice url={chat_url}/>}/>
         </Routes>
     );
 };

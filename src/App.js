8fff9dd136b37906c5479a621550e5a0296eb4dc
MandA508b <123251306+MandA508b@users.noreply.github.com>
2023-02-21 19:57:37 +0200

    changed server url (vps)

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Create from "./pages/Create";
 import Redact from "./pages/Redact";
 import YourPrice from "./pages/YourPrice/YourPrice";
 
-const url = 'https://ligabotv2.onrender.com'
+const url = 'https://ligabot.pp.ua'
 const chat_url = 'https://chat-server-zsxa.onrender.com'
 const App = () => {
     return (
